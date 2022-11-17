e26264335f0487de0e3e7c0863c12f7b91e28156
EnerioBalungcas <enerio.balungcas@gmail.com>
2022-11-17 21:28:59 +0800

    deleted droptable

--- app/Models/properties.php
+++ app/Models/properties.php
@@ -9,7 +9,7 @@ class Properties extends Model
 {
     use HasFactory;
     protected $table = 'properties_tbl';
-    protected $primarykey = 'property_id';
+    protected $primaryKey = 'property_id';
     protected $fillable = ['property_type_id', 'property_title', 'property_price', 'property_province_id', 'city_mun_id', 'floor_area', 'lot_area', 'property_description', 'owner_id', 'property_image_id' ];
 }
 
